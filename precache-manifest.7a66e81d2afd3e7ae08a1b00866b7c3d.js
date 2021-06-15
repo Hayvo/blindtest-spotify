01a522008acfc9f855b474fdbbde96ea631ed35e
Hayvo <pierre.denig@mines-paristech.fr>
2021-06-15 11:51:57 +0200

    Updates

---
+++ precache-manifest.7a66e81d2afd3e7ae08a1b00866b7c3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed39ba35b5ae35cab2e8eece193eadd",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "f4d95a36ebfb0d13c8ec",
+    "url": "/blindtest-spotify/static/css/main.a84f3191.chunk.css"
+  },
+  {
+    "revision": "80d69267c094783cf89b",
+    "url": "/blindtest-spotify/static/js/2.aeb8e54d.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.aeb8e54d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d95a36ebfb0d13c8ec",
+    "url": "/blindtest-spotify/static/js/main.ff8e6dce.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);
